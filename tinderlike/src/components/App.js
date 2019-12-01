68f110c59d28a65811a3cd9245ceb4227fcf07e6
Caio Lopes <caio.lopes@loggi.com>
2019-11-30 23:57:05 -0300

    add react-router

--- tinderlike/src/components/App.js
+++ tinderlike/src/components/App.js
@@ -2,11 +2,27 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import '../App.css';
 import Onboarding from './Onboarding';
+import Matcher from './Matcher';
 
-function App() {
-    return (
-      <Onboarding />
-    );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/matcher">
+          <Matcher />
+        </Route>
+        <Route path="/">
+          <Onboarding />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
